37a2d6626d21366cfb9b1f0e885abdb133802b73
Mark Cates <cates_mark@hotmail.com>
2019-05-08 06:19:20 -0500

    added authorization update and deletion of comments, must be current user and creator of comment to delete or update

--- routes/comments.js
+++ routes/comments.js
@@ -38,7 +38,7 @@ router.post("/", isLoggedIn, function(req, res) {
 });
 
 // comment edit route
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
   Comment.findById(req.params.comment_id, function(err, foundComment) {
     if (err) {
       res.redirect("back");
@@ -65,7 +65,7 @@ router.put("/:comment_id", function(req, res) {
 });
 
 // comment delete route
-router.delete("/:comment_id", function(req, res) {
+router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
   Comment.findOneAndDelete(req.params.comment_id, function(err) {
     if (err) {
       res.redirect("back");
@@ -80,6 +80,24 @@ function isLoggedIn(req, res, next) {
     return next();
   }
   res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+      if (err) {
+        res.redirect("back");
+      } else {
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
 }
 
 module.exports = router;
